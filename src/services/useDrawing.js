4fc40571e35e6c643c92d7a05df6bf0d1f2b1d1f
TomasBarnas93 <btomek506@gmail.com>
2023-10-19 14:41:36 +0200

    Vol-17.2

--- src/services/useDrawing.js
+++ src/services/useDrawing.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import { useState, useCallback } from 'react';
 
-const useDrawing = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const useDrawing = (color, lineThickness) => {
+  const [elements, setElements] = useState([]);
 
-export default useDrawing
+  const handleStart = useCallback((event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const { clientX, clientY } = event;
+    const id = elements.length;
+    const element = {
+      id,
+      type: 'pencil',
+      points: [{ x: clientX, y: clientY }],
+      color,
+      thickness: lineThickness,
+    };
+    setElements((prevState) => [...prevState, element]);
+  }, [color, lineThickness, elements]);
+
+  const handleMove = useCallback((event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const { clientX, clientY } = event;
+    const index = elements.length - 1;
+    const updatedElement = {
+      ...elements[index],
+      points: [...elements[index].points, { x: clientX, y: clientY }]
+    };
+    const updatedElements = [...elements];
+    updatedElements[index] = updatedElement;
+    setElements(updatedElements);
+  }, [elements]);
+
+  const handleEnd = useCallback((event) => {
+    event.preventDefault();
+    event.stopPropagation();
+  }, []);
+
+  return {
+    handleStart,
+    handleMove,
+    handleEnd,
+    elements,
+  };
+};
+
+export default useDrawing;
