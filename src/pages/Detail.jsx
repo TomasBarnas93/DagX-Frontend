6b5d526a44f1b7f5bc32805efd0a57f29e9085c5
TomasBarnas93 <btomek506@gmail.com>
2023-11-01 10:47:02 +0100

    Detail added carusel

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -12,6 +12,8 @@ import { useTranslation } from "react-i18next";
 import React, { useContext, useEffect } from "react";
 import { ImageContext } from "../services/ImageContext";
 import { SlArrowLeft } from "react-icons/sl";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 function Detail() {
   const { id } = useParams();
@@ -23,6 +25,12 @@ function Detail() {
     window.scrollTo(0, 0);
   }, []);
 
+   const galleryImages = image.details?.map(detail => ({
+    original: detail.subUrl,
+    thumbnail: detail.subUrl,
+    description: `Detail ${detail.id}`,
+  })) || [];
+
   return (
     <Flex
       flexDirection="column"
@@ -31,109 +39,45 @@ function Detail() {
       mt="10rem"
     >
       <Box
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      textAlign="center"
-      mb={4}
-      fontFamily="Poiret One"
-    >
-      <Image
-        className="imageWrapper"
-        src={image.url}
-        alt={image.name}
-        height={{ base: "30rem", md: "50rem" }}
-        margin="auto"
-      />
-      <Text fontSize="5xl" mt={2} fontWeight="semibold">
-        {image.name}
-      </Text>
-      <Text fontSize="3xl" mt={2} fontWeight="semibold">
-        {image.size}
-      </Text>
-      <Box
-        className="underlineCustom"
-        marginTop={{ base: "1.5rem", md: "2rem" }}
-      ></Box>
-      <Text fontSize="3xl" mt={5} width="60%">
-        {image.description}
-      </Text>
-      <Link to="/contact">
-        <Button fontSize="2xl" h="3rem" fontFamily="Poiret One" mt={10}>
-          {t("BtnAskForPrice")}
-        </Button>
-      </Link>
-    </Box>
-    <Flex
-  flexDirection={["column", "row"]}  
-  width={["100%", "85%"]}  
-  justifyContent="space-around"
-  mt="3rem"
->
-  {image.details?.[0] && (
-    <Image
-      src={image.details[0].subUrl}
-      alt="Detail 1"
-      height={["40rem", "50rem"]}  
-      className="imageWrapper"
-    />
-  )}
-  {image.details?.[0] && image.details?.[1] && (
-    <Box
-      width={["0", "2px"]} 
-      backgroundColor="black"
-      height="30rem"
-      alignSelf="center"
-      mx="7.5rem"
-      display={["none", "block"]}  
-    />
-  )}
-  {image.details?.[1] && (
-    <Image
-      src={image.details[1].subUrl}
-      alt="Detail 2"
-      height={["40rem", "50rem"]} 
-      className="imageWrapper"
-    />
-  )}
-</Flex>
-{image.details?.[2] ? (
-  <Flex 
-    gap={["0", "7rem"]}  
-    flexDirection={["column", "row"]}
-  >
-    <Image
-      className="imageWrapper"
-      src={image.details[2].subUrl}
-      alt="Detail 3"
-      height={["40rem", "50rem"]}
-      width={image.details.length === 4 ? "38rem" : "100%"}
-      mt="5rem"
-    />
-    {image.details?.[3] ? (
-      <>
-        <Box
-          width={["0", "2px"]}
-          backgroundColor="black"
-          height="30rem"
-          alignSelf="center"
-          mx="7.5rem"
-          marginTop="9rem"
-          display={["none", "block"]} 
-        />
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+        mb={4}
+        fontFamily="Poiret One"
+      >
         <Image
           className="imageWrapper"
-          src={image.details[3].subUrl}
-          alt="Detail 4"
-          height={["40rem", "50rem"]}
-          width="38rem"
-          mt="5rem"
+          src={image.url}
+          alt={image.name}
+          height={{ base: "30rem", md: "50rem" }}
+          margin="auto"
         />
-      </>
-    ) : null}
-  </Flex>
-) : null}
+        <Text fontSize="5xl" mt={2} fontWeight="semibold">
+          {image.name}
+        </Text>
+        <Text fontSize="3xl" mt={2} fontWeight="semibold">
+          {image.size}
+        </Text>
+        <Box
+          className="underlineCustom"
+          marginTop={{ base: "1.5rem", md: "2rem" }}
+        ></Box>
+        <Text fontSize="3xl" mt={5} width="60%">
+          {image.description}
+        </Text>
+        <Link to="/contact">
+          <Button fontSize="2xl" h="3rem" fontFamily="Poiret One" mt={10}>
+            {t("BtnAskForPrice")}
+          </Button>
+        </Link>
+      </Box>
+      <Box mt="3rem">
+      {galleryImages.length > 0 && (
+          <ImageGallery items={galleryImages} />
+        )}
+      </Box>
       <Box mt="3rem">
         <Link to="/projects">
           <IconButton
@@ -145,7 +89,8 @@ function Detail() {
         </Link>
       </Box>
     </Flex>
-  );
+);
+
 }
 
 export default Detail;
